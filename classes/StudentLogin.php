b423dbc5a5c641a75b95e24c8525020820f58fc1
shohorabpalash1233 <shohorabpalash1@gmail.com>
2017-09-22 11:14:36 +0600

    Login controller created for students

---
+++ classes/StudentLogin.php
@@ -0,0 +1,52 @@
+<?php
+	$filepath = realpath(dirname(__FILE__));
+	include_once ($filepath.'/../lib/Database.php');
+	include_once ($filepath.'/../helpers/Format.php');
+	include_once ($filepath.'/../lib/Session.php');
+	Session::init();
+?>
+<?php
+
+	class StudentLogin
+	{
+		private $db;
+		private $fm;
+
+		
+		public function __construct()
+		{
+			$this->db = new Database();
+			$this->fm = new Format();
+		}
+
+		public function StudentLogin($data){
+			$username 		= $this->fm->validation($data['username']);
+			$password 		= $this->fm->validation($data['password']);
+
+			$username 		= mysqli_real_escape_string($this->db->link, $data['username']);
+			$password 		= mysqli_real_escape_string($this->db->link, md5($data['password']));
+
+			if (empty($username) || empty($password)) {
+				$msg = "<span class='error'>Field must not be empty</span>";
+				return $msg;
+			}
+
+			$query = "SELECT * FROM tbl_registration WHERE username = '$username' AND password = '$password' ";
+			$result = $this->db->select($query);
+
+			if ($result != false) {
+				$value = $result->fetch_assoc();
+				Session::set("login", true);
+				Session::set("stuid", $value['id']);
+				Session::set("stuname", $value['username']);
+				header("Location: index.php");
+			} else {
+				$msg = "<span class='error'>Email and Password Not Matched</span>";
+				return $msg;
+			}
+		}
+
+
+
+	}
+?>
